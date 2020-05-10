80c4567c76651bd19700be01f2127910c65fd1f5
ma7inowy <jakubwachala@wp.pl>
2020-05-10 12:59:25 +0200

    Zadanie3 added

---
+++ Zadanie3/Main.py
@@ -0,0 +1,292 @@
+import os
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+from Sygnal import Sygnal
+from SygnalCiagly import SygnalCiagly
+from SygnalDyskretny import SygnalDyskretny
+
+
+class Main:
+
+    def wybor_sygnalu(self):
+        print("---SYGNALY---")
+        print("1. szum_o_rozkladzie_jednostajnym")
+        print("2. szum_gaussowski")
+        print("3. sygnal_sinusoidalny")
+        print("4. sygnal_sinusoidalny_wyprostowany_jednopolowkowo")
+        print("5. sygnal_sinusoidalny_wyprostowany_dwupolowkowo")
+        print("6. sygnal_prostokatny")
+        print("7. sygnal_prostokatny_symetryczny")
+        print("8. sygnal_trojkatny")
+        print("9. skok_jednostkowy")
+        print("10. impuls_jednostkowy")
+        print("11. szum_impulsowy")
+        print("------------------")
+
+    def menu_glowne(self, argument):
+        sc = SygnalCiagly()
+        sd = SygnalDyskretny()
+
+        amp = int(input('Podaj amplitude : '))
+        t1 = int(input('Podaj czas poczatkowy : '))
+        d = int(input('Podaj czas trwania sygnalu : '))
+
+        if argument == 1:
+            # ??? czy tak chce zrobic? czy zostawic ze rysowanie w SygnalCiagly????????????????
+            sc.szum_o_rozkladzie_jednostajnym(amp, t1, d).rysuj_sygnal()
+            # sc.szum_o_rozkladzie_jednostajnym(30, 0, 50),
+        elif argument == 2:
+            sc.szum_gaussowski(amp, t1, d).rysuj_sygnal()
+            # sc.szum_gaussowski(30, 0, 50),
+        elif argument == 3:
+            okres_T = int(input('Podaj okres podstawowy : '))
+            sc.sygnal_sinusoidalny(amp, okres_T, t1, d).rysuj_sygnal()
+            # sc.sygnal_sinusoidalny(10, 7, 0, 10),
+        elif argument == 4:
+            okres_T = int(input('Podaj okres podstawowy : '))
+            sc.sygnal_sinusoidalny_wyprostowany_jednopolowkowo(amp, okres_T, t1, d).rysuj_sygnal()
+            # sc.sygnal_sinusoidalny_wyprostowany_jednopolowkowo(10, 6, 0, 10),
+        elif argument == 5:
+            okres_T = int(input('Podaj okres podstawowy : '))
+            sc.sygnal_sinusoidalny_wyprostowany_dwupolowkowo(amp, okres_T, t1, d).rysuj_sygnal()
+            # sc.sygnal_sinusoidalny_wyprostowany_dwupolowkowo(10, 6, 0, 10),
+        elif argument == 6:
+            okres_T = int(input('Podaj okres podstawowy : '))
+            sc.sygnal_prostokatny(amp, okres_T, t1, d).rysuj_sygnal()
+            # sc.sygnal_prostokatny(10, 2, 0, 10),
+        elif argument == 7:
+            okres_T = int(input('Podaj okres podstawowy : '))
+            sc.sygnal_prostokatny_symetryczny(amp, okres_T, t1, d).rysuj_sygnal()
+            # sc.sygnal_prostokatny_symetryczny(10, 2, 0, 10),
+        elif argument == 8:
+            okres_T = int(input('Podaj okres podstawowy : '))
+            sc.sygnal_trojkatny(amp, okres_T, t1, d).rysuj_sygnal()
+            # sc.sygnal_trojkatny(10, 2, 0, 10),
+        elif argument == 9:
+            ts = int(input('Podaj współczynnik wypełnienia : '))
+            sc.skok_jednostkowy(amp, t1, d, ts).rysuj_sygnal()
+            # sc.skok_jednostkowy(10, -10, 20),
+        elif argument == 10:
+            sd.impuls_jednostkowy(amp, t1, d).rysuj_sygnal()
+            # sd.impuls_jednostkowy(1, -25, 50),
+        elif argument == 11:
+            p = int(input('Podaj prawdopodobienstwo : '))
+            sd.szum_impulsowy(amp, t1, d, p).rysuj_sygnal()
+            # sd.szum_impulsowy(1, 0, 50, 80)
+        else:
+            print("NACISNIETO ZLY PRZYCISK!!!")
+
+    def wczytaj_z_pliku(self, nazwa_pliku):
+        # w pliku poczatek i koniec przedzialu x'ow oraz wspolrzedne y
+        print("Wczytywanie z pliku...")
+        plik = open(nazwa_pliku)
+        caly_tekst = plik.read()
+        plik.close()
+        podzial_na_linie = caly_tekst.split('\n')
+        wartosci_y = podzial_na_linie[1].split(', ')  # wspolrzedne y
+        przedzial_wartosci_x = podzial_na_linie[0].split(', ')  # tablica z dwoma wartosciami tj.(pocz. i koniec)
+        ilosc_x = len(wartosci_y)
+
+        # konwersja tablicy str na float
+        for i in range(0, len(wartosci_y)):
+            wartosci_y[i] = float(wartosci_y[i])
+
+        wartosci_x = np.linspace(int(przedzial_wartosci_x[0]), int(przedzial_wartosci_x[1]), ilosc_x)  # wspolrzedne x
+        # plt.plot(wartosci_x, wartosci_y)
+        # plt.xlim(int(przedzial_wartosci_x[0]), int(przedzial_wartosci_x[1]))  # od do X
+        # plt.xlabel('t[s]')
+        # plt.ylabel('Amplituda')
+        # plt.show()
+
+        return Sygnal(wartosci_x, wartosci_y)
+
+    def zapisz_do_pliku(self, sygnal, nazwa_pliku):
+        poczatek_przedzialu = int(sygnal.wartosci_x[0])
+        koniec_przedzialu = int(sygnal.wartosci_x[len(sygnal.wartosci_x) - 1])
+        wartosci_y = sygnal.wartosci_y
+
+        print("Zapisywanie do pliku...")
+        plik = open(nazwa_pliku, "w")
+        przecinek = ", "
+        tekst = str(poczatek_przedzialu) + przecinek + str(koniec_przedzialu)
+        plik.write(tekst)
+        plik.write("\n")
+        plik.write(self.zamien_liste_w_str(wartosci_y))
+        plik.close()
+
+    def zamien_liste_w_str(self, tablica1):
+        stringg = ""
+        for i in range(len(tablica1)):
+            if i != len(tablica1) - 1:
+                stringg = stringg + str(tablica1[i]) + ", "
+            else:
+                stringg = stringg + str(tablica1[i])
+        return stringg
+
+
+if __name__ == '__main__':
+    main = Main()
+    # main.wybor_sygnalu()
+    # inp = input('Podaj jaki sygnal chcesz rozpatrzec : ')
+    # # os.system('cls')  # powinno czyscic ale moze zrobi tylko jak zrobbimy z tego skrypt
+    # main.menu_glowne(int(inp))
+
+    sc = SygnalCiagly()
+    # dzialania na wykresach
+    # sc.szum_o_rozkladzie_jednostajnym(30, 0, 50).dzielenie(sc.sygnal_prostokatny(10, 2, 0, 10)).rysuj_sygnal()
+    # print(sc.szum_o_rozkladzie_jednostajnym(30, 0, 50).wariancja())
+    # print(sc.szum_o_rozkladzie_jednostajnym(30, 0, 50).wariancja())
+    # main.wczytaj_z_pliku()
+    # tablica = [0, 1, 2, 3, 4]
+    # main.zapisz_do_pliku(0, 10, tablica)
+    # sc.sygnal_trojkatny(10, 2, 0, 10).rysuj_histogram(5)
+    # main.wczytaj_z_pliku()
+    # x = np.linspace(t1, t1 + d, 1000)
+    # plt.plot(syg.wartosci_x, syg.wartosci_y)
+    # plt.xlim(self.czas_poczatkowy, self.czas_poczatkowy + self.czas_trwania_sygnalu)  # od do X
+    # # plt.xlim(self.wartosci_x[0], self.wartosci_x[len(self.wartosci_x)-1])  # od do X
+    # plt.xlabel('t[s]')
+    # plt.ylabel('Amplituda')
+    # plt.show()
+
+    # sc.sygnal_prostokatny(15, 5, 0, 20).rysuj_sygnal()
+    # sc.sygnal_prostokatny(15, 5, 0, 20).rysuj_histogram(15)
+    # sc.sygnal_prostokatny(15, 5, 0, 20).pokazWynikiParametrow()
+
+    # sc.sygnal_sinusoidalny(15, 5, 0, 20).rysuj_histogram(15)
+    # sc.sygnal_sinusoidalny(15, 5, 0, 20).pokazWynikiParametrow()
+    # sc.sygnal_sinusoidalny(15, 5, 0, 20).rysuj_sygnal()
+
+    # sc.sygnal_trojkatny(15, 5, 0, 20).rysuj_sygnal()
+    # sc.sygnal_trojkatny(15, 5, 0, 20).rysuj_histogram(15)
+    # sc.sygnal_trojkatny(15, 5, 0, 20).pokazWynikiParametrow()
+
+    sd = SygnalDyskretny()
+    # sd.szum_impulsowy(15, 0, 20, 70).rysuj_sygnal()
+    # sd.szum_impulsowy(15, 0, 20, 70).rysuj_histogram(15)
+    # sd.szum_impulsowy(15, 0, 20, 70).pokazWynikiParametrow()
+    # sc.skok_jednostkowy(10, 0, 20, 3).rysuj_sygnal()
+    # syg = sc.sygnal_sinusoidalny(15, 5, 0, 20).dodawanie(sc.sygnal_prostokatny(15, 5,0,20))
+    # syg.rysuj_sygnal()
+    # syg.rysuj_histogram(15)
+    # syg.pokazWynikiParametrow()
+
+    # syg = sc.sygnal_prostokatny(15, 5, 0, 20).odejmowanie(sc.sygnal_trojkatny(15, 5, 0, 20))
+    # syg.rysuj_sygnal()
+    # syg.rysuj_histogram(15)
+    # syg.pokazWynikiParametrow()
+
+    # syg = sc.sygnal_sinusoidalny(15, 5, 0, 20).mnozenie(sc.sygnal_trojkatny(15, 5, 0, 20))
+    # syg.rysuj_sygnal()
+    # syg.rysuj_histogram(15)
+    # syg.pokazWynikiParametrow()
+    #
+    # syg = sc.sygnal_sinusoidalny(15, 5, 0, 20).dzielenie(sc.sygnal_prostokatny(15, 5, 0, 20))
+    # syg.rysuj_sygnal()
+    # syg.rysuj_histogram(15)
+    # syg.pokazWynikiParametrow()
+
+    # sc.sygnal_sinusoidalny_wyprostowany_jednopolowkowo(5, 10, -5, 15).rysuj_sygnal()
+    # sc.sygnal_sinusoidalny_wyprostowany_jednopolowkowo(5, 10, -5, 15).pokazWynikiParametrow()
+    # 1szum_o_rozk_jednost
+    # 2szum_gauss
+    # 3syg_sinus
+
+    # syg = sc.sygnal_sinusoidalny(15, 5, 0, 20).pokazWynikiParametrow()
+    # print(syg)
+    # sc.sygnal_trojkatny(15, 5, 0, 20).kwantyzacja(10, 5).rysuj_sygnal()
+    # sc.sygnal_trojkatny(16, 5, 6, 30).rysuj_sygnal()
+
+    # sc.sygnal_trojkatny(10, 10, 1, 30).kwantyzacja(100, 15).rysuj_sygnal()
+    # syg_prob = sc.sygnal_trojkatny(15, 5, 0, 20).ekstrapolacja_zerowego_rzeduNaj(88).rysuj_sygnal()
+    print("XD")
+    # print(len(sc.sygnal_trojkatny(15, 5, 0, 20).kwantyzacja(70, 5).wartosci_y))
+    # print(len(sc.sygnal_trojkatny(15, 5, 0, 20).probkowanie(70).wartosci_y))
+    ##ZADANIE 2
+
+    # syg = sc.sygnal_sinusoidalny(15, 5, 0, 20)
+    # y = syg.wartosci_y
+    # x = syg.wartosci_x
+    #
+    # y2 = sc.sygnal_sinusoidalny(15, 5, 0, 20).kwantyzacja(200, 5).wartosci_y
+    # x2 = sc.sygnal_sinusoidalny(15, 5, 0, 20).kwantyzacja(200, 5).wartosci_x
+    #
+    # plt.plot(x, y, color='red')
+    # plt.plot(x2, y2)
+    # # plt.xlim(self.wartosci_x[0], self.wartosci_x[len(self.wartosci_x) - 1])  # od do X
+    # plt.xlabel('t[s]')
+    # plt.ylabel('Amplituda')
+    # plt.show()
+    #
+    # Sygnal.pokaz_wyniki_miar(syg.probkowanie(200),sc.sygnal_sinusoidalny(15, 5, 0, 20).kwantyzacja(200, 5))
+
+    # syg = sc.sygnal_prostokatny(15, 5, 0, 20)
+    # y = syg.wartosci_y
+    # x = syg.wartosci_x
+
+    # y2 = sc.sygnal_prostokatny(15, 5, 0, 20).rekonstrukcja_w_oparciu_o_fun_sinc(200).wartosci_y
+    # x2 = sc.sygnal_prostokatny(15, 5, 0, 20).rekonstrukcja_w_oparciu_o_fun_sinc(200).wartosci_x
+    #
+    # plt.plot(x, y, color='red')
+    # plt.plot(x2, y2)
+    # # plt.xlim(self.wartosci_x[0], self.wartosci_x[len(self.wartosci_x) - 1])  # od do X
+    # plt.xlabel('t[s]')
+    # plt.ylabel('Amplituda')
+    # plt.show()
+    print("oryginal")
+    # print(syg.probkowanie(200).wartosci_y)
+    #
+    # print(sc.sygnal_prostokatny(15, 5, 0, 20).kwantyzacja(200, 5).wartosci_y)
+    # Sygnal.pokaz_wyniki_miar(syg.probkowanie(200), sc.sygnal_prostokatny(15, 5, 0, 20).kwantyzacja(200, 5)) #wychodzi 0?
+    #
+    # # syg = sc.sygnal_trojkatny(15, 5, 0, 20)
+    # # y = syg.wartosci_y
+    # # x = syg.wartosci_x
+    # #
+    # syg_prob = sc.sygnal_prostokatny(15, 5, 0, 20).kwantyzacja(200,5)
+    # syg_prob.sygDyskretny = True
+    # y2 = syg_prob.wartosci_y
+    # x2 = syg_prob.wartosci_x
+    # #
+    # # y2 = syg.interpolacja_pierwszego_rzeduNaj(200).wartosci_y
+    # # x2 = syg.interpolacja_pierwszego_rzeduNaj(200).wartosci_x
+    # #
+    # plt.plot(x, y, color='red')
+    # plt.plot(x2, y2)
+    # # plt.xlim(self.wartosci_x[0], self.wartosci_x[len(self.wartosci_x) - 1])  # od do X
+    # plt.xlabel('t[s]')
+    # plt.ylabel('Amplituda')
+    # plt.show()
+    #
+    # Sygnal.pokaz_wyniki_miar(syg.probkowanie(200), sc.sygnal_trojkatny(15, 5, 0, 20).kwantyzacja(200, 5))
+    # syg2 = sc.sygnal_sinusoidalny(2, 0.01, 0, 1) #100hz
+    # syg = sc.sygnal_sinusoidalny(2, 0.01, 0, 1).rekonstrukcja_w_oparciu_o_fun_sinc(101)
+    # # syg_kwant = sc.sygnal_sinusoidalny(15, 5, 0, 20).kwantyzacja(400, 8)
+    # # Sygnal.pokaz_wyniki_miar(syg_probkowany.probkowanie(100), syg_kwant)  # wychodzi 0?
+    #
+    # y = syg.wartosci_y
+    # x = syg.wartosci_x
+    # #
+    # # y2 = syg_kwant.wartosci_y
+    # # x2 = syg_kwant.wartosci_x
+    # #
+    # y2 = syg2.wartosci_y
+    # x2 = syg2.wartosci_x
+    # #
+    # plt.plot(x, y, color='red')
+    # plt.plot(x2, y2)
+    # # plt.xlim(self.wartosci_x[0], self.wartosci_x[len(self.wartosci_x) - 1])  # od do X
+    # plt.xlabel('t[s]')
+    # plt.ylabel('Amplituda')
+    # plt.show()
+    #
+    # print(y)
+    # print(x)
+    # print(len(x2))
+    # print(len(y2))
+    syg = sc.sygnal_sinusoidalny(15, 5, 0, 20).kwantyzacja(200,8)
+    syg.rysuj_sygnal()
+    # syg.rysuj_histogram(15)
+    syg.pokazWynikiParametrow()
---
+++ Zadanie3/Sygnal.py
@@ -0,0 +1,329 @@
+import math
+import numpy as np
+from sortedcontainers import SortedSet
+import matplotlib.pyplot as plt
+import cmath
+
+skok_probkowania = 0
+
+
+class Sygnal:
+
+    def __init__(self, x, y):
+        self.sygDyskretny = False
+        self.wartosci_x = x
+        self.wartosci_y = y
+        # self.t1 = 0
+        # self.d = 0
+
+    # dodaje dany sygnal z innym
+    def dodawanie(self, sygnal2):
+        y = []
+        for i in range(len(self.wartosci_y)):
+            # x[i] = self.wartosci_x[i] + sygnal2.wartosc_x[i]
+            y.append(self.wartosci_y[i] + sygnal2.wartosci_y[i])
+        sygnal_nowy = Sygnal(self.wartosci_x, y)
+        return sygnal_nowy
+
+    def odejmowanie(self, sygnal2):
+        y = []
+        for i in range(len(self.wartosci_y)):
+            # x[i] = self.wartosci_x[i] + sygnal2.wartosc_x[i]
+            y.append(self.wartosci_y[i] - sygnal2.wartosci_y[i])
+        sygnal_nowy = Sygnal(self.wartosci_x, y)
+        return sygnal_nowy
+
+    def mnozenie(self, sygnal2):
+        y = []
+        for i in range(len(self.wartosci_y)):
+            # x[i] = self.wartosci_x[i] + sygnal2.wartosc_x[i]
+            y.append(self.wartosci_y[i] * sygnal2.wartosci_y[i])
+        sygnal_nowy = Sygnal(self.wartosci_x, y)
+        return sygnal_nowy
+
+    # jak 0 to 1 ??? mozna tak?
+    def dzielenie(self, sygnal2):
+        y = []
+        for i in range(len(self.wartosci_y)):
+            # x[i] = self.wartosci_x[i] + sygnal2.wartosc_x[i]
+            if sygnal2.wartosci_y[i] == 0:
+                y.append(self.wartosci_y[i] / 1)
+            else:
+                y.append(self.wartosci_y[i] / sygnal2.wartosci_y[i])
+        sygnal_nowy = Sygnal(self.wartosci_x, y)
+        return sygnal_nowy
+
+    def rysuj_sygnal(self):
+        if not self.sygDyskretny:
+            plt.plot(self.wartosci_x, self.wartosci_y)
+            plt.xlim(self.wartosci_x[0], self.wartosci_x[len(self.wartosci_x) - 1])  # od do X
+            plt.xlabel('t[s]')
+            plt.ylabel('Amplituda')
+            plt.show()
+        else:
+            plt.scatter(self.wartosci_x, self.wartosci_y)
+            plt.xlim(self.wartosci_x[0], self.wartosci_x[len(self.wartosci_x) - 1])  # od do X
+            plt.xlabel('t[s]')
+            plt.ylabel('Amplituda')
+            plt.show()
+
+    def rysuj_histogram(self, ilosc_przedzialow):
+        plt.hist(self.wartosci_y, bins=int(ilosc_przedzialow), rwidth=0.85)
+        plt.show()
+
+    def wartosc_srednia(self):
+        ulamek = 1 / (len(self.wartosci_x) - 0 + 1)
+        suma = 0
+        for i in range(len(self.wartosci_y)):
+            suma += self.wartosci_y[i]
+        wynik = ulamek * suma
+        # print("Wartosc srednia to: ", wynik)
+        return wynik
+
+    def wartosc_bezwzgledna(self):
+        # ulamek = 1 / (self.wartosci_x[len(self.wartosci_x) - 1] - self.wartosci_x[0] + 1)
+        ulamek = 1 / (len(self.wartosci_x) - 0 + 1)
+        suma = 0
+        for i in range(len(self.wartosci_x)):
+            suma += math.fabs(self.wartosci_y[i])
+        wynik = ulamek * suma
+        # print("Wartosc srednia bezwzgledna to: ", wynik)
+        return wynik
+
+    def wartosc_skuteczna(self):
+        return math.sqrt(self.moc_srednia())
+
+    def wariancja(self):
+        # ulamek = 1 / (self.wartosci_x[len(self.wartosci_x) - 1] - self.wartosci_x[0] + 1)
+        ulamek = 1 / (len(self.wartosci_x) - 0 + 1)
+        suma = 0
+        for i in range(len(self.wartosci_x)):
+            suma += (self.wartosci_y[i] - self.wartosc_srednia()) ** 2
+        wynik = ulamek * suma
+        # print("Wartosc wariancji to : ", wynik)
+        return wynik
+
+    def moc_srednia(self):
+        ulamek = 1 / (len(self.wartosci_x) - 0 + 1)
+        suma = 0
+        for i in range(len(self.wartosci_x)):
+            suma += self.wartosci_y[i] ** 2  # x^2(n) to x(n) * x(n) no nie?
+        wynik = ulamek * suma
+        # print("Wartosc mocy sredniej to: ", wynik)
+        return wynik
+
+    def pokazWynikiParametrow(self):
+        str1 = "Wartosc srednia to: " + str(self.wartosc_srednia())
+        str2 = "\nWartosc bezwzględna to: " + str(self.wartosc_bezwzgledna())
+        str3 = "\nWartosc skuteczna to: " + str(self.wartosc_skuteczna())
+        str4 = "\nWartosc wariancji to: " + str(self.wariancja())
+        str5 = "\nWartosc mocy średniej to: " + str(self.moc_srednia())
+
+        return str1 + str2 + str3 + str4 + str5
+
+    # DO ZADANIA2
+
+    def probkowanie(self, czestotliwosc):
+        x = []
+        y = []
+        wartosc = int(1000 / czestotliwosc)
+        global skok_probkowania
+        skok_probkowania = wartosc
+        # print("WARTOSC" + str(wartosc))
+        for i in range(0, 1000, wartosc):
+            # print(i)
+            x.append(self.wartosci_x[i])
+            y.append(self.wartosci_y[i])
+        sygnal = Sygnal(x, y)
+        sygnal.sygDyskretny = True
+        # print(sygnal.wartosci_x)
+        return sygnal
+
+    def znajdz_max_z_przedzialu(self, lista, poczatek, koniec):
+        temp_list = []
+        for i in range(len(lista)):
+            if poczatek <= lista[i] <= koniec:
+                temp_list.append(lista[i])
+
+        nowy_y = max(temp_list)
+
+        return nowy_y
+
+    # COS JEST NIE TAK!
+    def kwantyzacja(self, czestotliwosc, poziom_kwantyzacji):
+
+        syg_probkowany = self.probkowanie(czestotliwosc)
+
+        syg_probkowany.sygDyskretny = False
+        syg_probkowany.sygDyskretny = False
+        minn = syg_probkowany.wartosci_y[0]
+        maxx = syg_probkowany.wartosci_y[0]
+        maxx = syg_probkowany.wartosci_y[0]
+        for i in range(len(syg_probkowany.wartosci_y)):
+            if minn > syg_probkowany.wartosci_y[i]:
+                minn = syg_probkowany.wartosci_y[i]
+            if maxx < syg_probkowany.wartosci_y[i]:
+                maxx = syg_probkowany.wartosci_y[i]
+
+        roznica = maxx - minn
+        lista = []
+
+        for i in range(poziom_kwantyzacji + 1):
+            lista.append(minn + ((roznica / poziom_kwantyzacji) * i))
+
+        lista.sort()
+
+        nowe_y = [min(lista, key=lambda t_v: abs(t_v - v)) for v in syg_probkowany.wartosci_y]
+
+        syg_probkowany.wartosci_y = nowe_y
+
+        return syg_probkowany
+
+    def rect(self, t):
+        if np.math.fabs(t) > 0.5:
+            return 0
+        elif np.math.fabs(t) == 0.5:
+            return 0.5
+        else:
+            return 1
+
+    # cos moze byc nie tak, bo dziwne te krzeselka wychodza
+    def ekstrapolacja_zerowego_rzeduNaj(self, czestotliwosc_probkowania):
+        tablica_nowych_y = []
+        tablica_nowych_x = []
+        syg_probkowany = self.probkowanie(czestotliwosc_probkowania)
+        syg_probkowany.sygDyskretny = False
+        duze_t = (syg_probkowany.wartosci_x[1] - syg_probkowany.wartosci_x[0])  # krok sygnalu
+        t = syg_probkowany.wartosci_x[0]
+        while t < syg_probkowany.wartosci_x[-1]:
+            suma = 0.0
+            for n in range(len(syg_probkowany.wartosci_x)):
+                srodek_rect = (t - (duze_t / 2) - (n * duze_t)) / duze_t
+                suma += syg_probkowany.wartosci_y[n] * syg_probkowany.rect(srodek_rect)
+            tablica_nowych_y.append(suma)
+            tablica_nowych_x.append(t)
+            t += 1 / czestotliwosc_probkowania
+
+        syg_probkowany.wartosci_x = tablica_nowych_x
+        syg_probkowany.wartosci_y = tablica_nowych_y
+
+        return syg_probkowany
+
+    # TEN WZOR TEZ OKEJ ALE NIE JEST ROBIONE ZE WZORU
+
+    # def ekstrapolacja_zerowego_rzedu(self, czestotliwosc):
+    #     wsp_x = []
+    #     wsp_y = []
+    #     czest = 1 / (czestotliwosc * 100)
+    #     syg_ekstr_zero = self.probkowanie(czestotliwosc)
+    #     syg_ekstr_zero.sygDyskretny = False
+    #     krokSygnalu = syg_ekstr_zero.wartosci_x[1] - syg_ekstr_zero.wartosci_x[0]
+    #     # for i in range(0, krokSygnalu, czest):  # przeskok na kolejny y
+    #     #     for j in range(i, syg_probkowany.wartosci_x[-1], czest):
+    #     #         wsp_x.append(syg_probkowany.wartosci_x[i])
+    #     #         wsp_y.append(syg_probkowany.wartosci_y[i])
+    #     for i in range(len(syg_ekstr_zero.wartosci_x)):
+    #         licznik = syg_ekstr_zero.wartosci_x[i]
+    #         while licznik < syg_ekstr_zero.wartosci_x[i] + krokSygnalu:
+    #             # for j in range(syg_probkowany.wartosci_x[i], syg_probkowany.wartosci_x[i + 1], czest):
+    #             wsp_x.append(licznik)
+    #             wsp_y.append(syg_ekstr_zero.wartosci_y[i])
+    #             licznik += czest
+    #     syg_ekstr_zero.wartosci_x = wsp_x
+    #     syg_ekstr_zero.wartosci_y = wsp_y
+    #
+    #     return syg_ekstr_zero
+
+    def tri(self, t):
+        return 0 if np.math.fabs(t) > 1 else 1 - np.math.fabs(t)
+
+    def interpolacja_pierwszego_rzeduNaj(self, czestotliwosc):
+        syg_int_pierw_rzedu = self.probkowanie(czestotliwosc)
+        syg_int_pierw_rzedu.sygDyskretny = False
+        duze_t = syg_int_pierw_rzedu.wartosci_x[1] - syg_int_pierw_rzedu.wartosci_x[0]  # krok sygnalu
+        tablica_nowych_x = []
+        tablica_nowych_y = []
+        t = syg_int_pierw_rzedu.wartosci_x[0]
+
+        while t < syg_int_pierw_rzedu.wartosci_x[-1]:
+            suma = 0.0
+            for i in range(len(syg_int_pierw_rzedu.wartosci_x)):
+                suma += syg_int_pierw_rzedu.wartosci_y[i] * syg_int_pierw_rzedu.tri((t - i * duze_t) / duze_t)
+            tablica_nowych_x.append(t)
+            tablica_nowych_y.append(suma)
+            t += 1 / czestotliwosc
+
+        syg_int_pierw_rzedu.wartosci_x = tablica_nowych_x
+        syg_int_pierw_rzedu.wartosci_y = tablica_nowych_y
+
+        return syg_int_pierw_rzedu
+
+    @staticmethod
+    def sinc(t):
+        if t == 0:
+            return 1
+        else:
+            return math.sin(math.pi * t) / (math.pi * t)
+
+    def rekonstrukcja_w_oparciu_o_fun_sinc(self, czestotliwosc):
+        syg_probkowany = self.probkowanie(czestotliwosc)
+
+        przedzial_czasowy = syg_probkowany.wartosci_x[1] - syg_probkowany.wartosci_x[0]
+        nowa_lista_y = []
+        nowa_lista_x = []
+        t = syg_probkowany.wartosci_x[0]
+        # while t < syg_probkowany.wartosci_x[-1] + (1.0 / (1.5 * czestotliwosc)):
+        while t < syg_probkowany.wartosci_x[-1]:
+            sum = 0.0
+            for i in range(len(syg_probkowany.wartosci_x)):
+                arg = t / przedzial_czasowy - i
+                sum += syg_probkowany.wartosci_y[i] * Sygnal.sinc(arg)
+            nowa_lista_x.append(t)
+            nowa_lista_y.append(sum)
+            t += 1 / czestotliwosc
+
+        return Sygnal(nowa_lista_x, nowa_lista_y)
+
+    # syg probkowany do syg kwantowanego?
+    @staticmethod
+    def blad_sredniokwadratowy(syg_oryginalny, syg_kwantowany):
+
+        ilosc_pktow = len(syg_kwantowany.wartosci_x)
+
+        suma = 0
+        for i in range(ilosc_pktow):
+            suma += (syg_kwantowany.wartosci_y[i] - syg_oryginalny.wartosci_y[i]) ** 2
+            print(suma)
+
+        return suma / ilosc_pktow
+
+    # syg probkowany do syg kwantowanego?
+    @staticmethod
+    def stosunek_sygnal_szum(syg_oryginalny, syg_kwantowany):
+        licznik = 0
+        mianownik = 0
+        for i in range(len(syg_oryginalny.wartosci_y)):
+            licznik += syg_oryginalny.wartosci_y[i] ** 2
+            mianownik += (syg_oryginalny.wartosci_y[i] - syg_kwantowany.wartosci_y[i]) ** 2
+
+        wynik = 10 * cmath.log(licznik / mianownik, 10)
+
+        return wynik
+
+    @staticmethod
+    def maksymalna_roznica(syg_oryginalny, syg_kwantowany):
+        max = math.fabs(syg_oryginalny.wartosci_y[0] - syg_kwantowany.wartosci_y[0])
+        for i in range(1, len(syg_oryginalny.wartosci_y)):
+            if max < math.fabs(syg_oryginalny.wartosci_y[i] - syg_kwantowany.wartosci_y[i]):
+                max = math.fabs(syg_oryginalny.wartosci_y[i] - syg_kwantowany.wartosci_y[i])
+        return max
+
+    @staticmethod
+    def pokaz_wyniki_miar(syg_oryginalny, syg_kwantowany):
+        str1 = "blad_sredniokwadratowy: " + str(Sygnal.blad_sredniokwadratowy(syg_oryginalny, syg_kwantowany))
+        str2 = "\nstosunek_sygnal_szum: " + str(Sygnal.stosunek_sygnal_szum(syg_oryginalny, syg_kwantowany))
+        str3 = "\nmaksymalna_roznica: " + str(Sygnal.maksymalna_roznica(syg_oryginalny, syg_kwantowany))
+        return str1 + str2 + str3
+
+
+
